9884f14e75385d4f57d27afc83332b4e0e82f863
polarpia-tech <polarpia@gmail.com>
2026-05-27 01:44:38 +0200

    Update charts.js

--- ​js/charts.js
+++ ​js/charts.js
@@ -1,19 +1,12 @@
 let chartInstance = null;
-
-export function renderChart(canvasId, totalData) {
+export function renderChart(canvasId, dataArray) {
     const ctx = document.getElementById(canvasId).getContext('2d');
-    
-    // Καταστροφή προηγούμενου γραφήματος αν υπάρχει
     if (chartInstance) chartInstance.destroy();
-    
     chartInstance = new Chart(ctx, {
         type: 'doughnut',
         data: {
             labels: ['Cash', 'POS', 'FREE', 'Uber'],
-            datasets: [{
-                data: totalData, // Πίνακας με τα ποσά [cash, cards, freenow, uber]
-                backgroundColor: ['#34d399', '#eab308', '#d97706', '#60a5fa']
-            }]
+            datasets: [{ data: dataArray, backgroundColor: ['#34d399', '#eab308', '#d97706', '#60a5fa'] }]
         },
         options: { responsive: true, plugins: { legend: { display: true } } }
     });
