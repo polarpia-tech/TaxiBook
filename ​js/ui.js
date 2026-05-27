dec10753cfe1b0ea50c4c35743f9d7f8d4755b58
polarpia-tech <polarpia@gmail.com>
2026-05-27 02:33:48 +0200

    Delete ui.js

--- ​js/ui.js
+++
@@ -1,9 +0,0 @@
-export function toggleVisibility(elementId, isVisible) {
-    const el = document.getElementById(elementId);
-    if (el) isVisible ? el.classList.remove('hidden') : el.classList.add('hidden');
-}
-
-export function updateDashboard(total) {
-    const el = document.getElementById('dashGross');
-    if (el) el.textContent = total.toFixed(2) + " €";
-}
